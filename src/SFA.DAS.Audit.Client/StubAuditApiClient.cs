11775683e3f38b4411621ca6aa362d5639f1d64a
Simon Bull <sbulluk@users.noreply.github.com>
2017-01-17 10:41:38 +0000

    Add stub imlementation for Api Client for testing

---
+++ src/SFA.DAS.Audit.Client/StubAuditApiClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SFA.DAS.Audit.Types;
+
+namespace SFA.DAS.Audit.Client
+{
+    public class StubAuditApiClient : IAuditApiClient
+    {
+        private readonly string _outputDirectory;
+
+        public StubAuditApiClient(string outputDirectory)
+        {
+            _outputDirectory = outputDirectory;
+        }
+
+        public async Task Audit(AuditMessage message)
+        {
+            if (!Directory.Exists(_outputDirectory))
+            {
+                Directory.CreateDirectory(_outputDirectory);
+            }
+
+            var filePrefix = DateTime.Now.ToString("yyyy-MM-dd-HHmmss");
+            var fileCounter = 1;
+            var path = Path.Combine(_outputDirectory, $"{filePrefix}-{fileCounter:000}.json");
+            while (File.Exists(path))
+            {
+                fileCounter++;
+                path = Path.Combine(_outputDirectory, $"{filePrefix}-{fileCounter:000}.json");
+            }
+
+            using (var stream = new FileStream(path, FileMode.CreateNew, FileAccess.Write))
+            using (var writer = new StreamWriter(stream))
+            {
+                await writer.WriteAsync(JsonConvert.SerializeObject(message, Formatting.Indented));
+                await writer.FlushAsync();
+                writer.Close();
+            }
+        }
+    }
+}
