16da106b86022843dfda43b9cf59219c7c9fec3c
Simon Bull <sbulluk@users.noreply.github.com>
2017-01-20 15:39:23 +0000

    Check for bad configuration in api client

--- src/SFA.DAS.Audit.Client/AuditApiClient.cs
+++ src/SFA.DAS.Audit.Client/AuditApiClient.cs
@@ -11,6 +11,31 @@ namespace SFA.DAS.Audit.Client
 
         public AuditApiClient(AuditApiConfiguration configuration)
         {
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+            if (string.IsNullOrEmpty(configuration.ApiBaseUrl))
+            {
+                throw new NullReferenceException("configuration.ApiBaseUrl is not specified");
+            }
+            if (string.IsNullOrEmpty(configuration.ClientId))
+            {
+                throw new NullReferenceException("configuration.ClientId is not specified");
+            }
+            if (string.IsNullOrEmpty(configuration.ClientSecret))
+            {
+                throw new NullReferenceException("configuration.ClientSecret is not specified");
+            }
+            if (string.IsNullOrEmpty(configuration.Tenant))
+            {
+                throw new NullReferenceException("configuration.Tenant is not specified");
+            }
+            if (string.IsNullOrEmpty(configuration.IdentifierUri))
+            {
+                throw new NullReferenceException("configuration.IdentifierUri is not specified");
+            }
+
             _configuration = configuration;
             _httpClient = new SecureHttpClient(configuration);
         }
