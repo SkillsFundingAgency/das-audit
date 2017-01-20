408626e3e579bca32a33e996629e2cf9dcc82121
Simon Bull <sbulluk@users.noreply.github.com>
2017-01-20 16:41:30 +0000

    Make logging configurable from cloud

--- src/SFA.DAS.Audit.Web/Global.asax.cs
+++ src/SFA.DAS.Audit.Web/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Http;
+using SFA.DAS.Audit.Infrastructure.Logging;
 
 namespace SFA.DAS.Audit.Web
 {
@@ -10,7 +11,8 @@ namespace SFA.DAS.Audit.Web
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
+            LoggingConfig.ConfigureLogging();
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);            
