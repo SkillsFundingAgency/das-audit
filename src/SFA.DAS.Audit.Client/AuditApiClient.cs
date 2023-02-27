bf1265bf43445b3e97f5482f929ec5fe70c9f58c
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-02-27 13:55:18 +0000

    allow empty clientId, secret and tenant for MI connection

--- src/SFA.DAS.Audit.Client/AuditApiClient.cs
+++ src/SFA.DAS.Audit.Client/AuditApiClient.cs
@@ -19,18 +19,6 @@ namespace SFA.DAS.Audit.Client
             {
                 throw new NullReferenceException("configuration.ApiBaseUrl is not specified");
             }
-            if (string.IsNullOrEmpty(configuration.ClientId))
-            {
-                throw new NullReferenceException("configuration.ClientId is not specified");
-            }
-            if (string.IsNullOrEmpty(configuration.ClientSecret))
-            {
-                throw new NullReferenceException("configuration.ClientSecret is not specified");
-            }
-            if (string.IsNullOrEmpty(configuration.Tenant))
-            {
-                throw new NullReferenceException("configuration.Tenant is not specified");
-            }
             if (string.IsNullOrEmpty(configuration.IdentifierUri))
             {
                 throw new NullReferenceException("configuration.IdentifierUri is not specified");
